f9b4496eb7efccc9486256b4f5632872f74e33bf
Chinmayee Garnepudi <24jr1a4412gdg@gmail.com>
2026-01-02 15:43:53 +0000

    not working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Literata', 'serif'],
+        body: ['sans-serif'],
+        headline: ['serif'],
         code: ['monospace'],
       },
       colors: {
